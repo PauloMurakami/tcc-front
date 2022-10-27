638c4ea8b58ed3b280501612f07c8630f79f5813
Paulo Murakami <ppmura13@gmail.com>
2022-10-26 22:47:50 -0300

    First commit

---
+++ src/app/_services/event.service.ts
@@ -0,0 +1,13 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '@environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class EventServices {
+    constructor(private http: HttpClient) { }
+
+    getAllOpen() {
+        return this.http.get<any[]>(`${environment.apiUrl}/find-events-open`);
+    }
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+﻿import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '@app/_models';
+import { EventServices } from '@app/_services';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    loading = false;
+    eventos: any[];
+
+    constructor(private eventosService: EventServices) { }
+
+    ngOnInit() {
+        this.eventosService.getAllOpen().subscribe(eventos => {
+            this.loading = false;
+            this.eventos = eventos;
+        });
+    }
+}
