312a1c4abc7960f7c3c6bfbeb4b532b33268785e
Paulo Murakami <ppmura13@gmail.com>
2023-02-15 18:03:25 -0300

    subindo tudo

---
+++ src/app/_services/professor.service.ts
@@ -0,0 +1,22 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '@environments/environment';
+export interface BodyCreateProfessor {
+    email: string
+    nome: string
+    senha: string
+}
+@Injectable({ providedIn: 'root' })
+export class ProfessorService {
+    constructor(private http: HttpClient) { }
+    createProfessor(body: BodyCreateProfessor) {
+        return this.http.post<any>(`${environment.apiUrl}/register-teacher`, body)
+    }
+    getAllOpen() {
+        return this.http.get<any[]>(`${environment.apiUrl}/find-events-open`);
+    }
+    // {
+
+    // }
+}
--- src/app/faculdade/cadastrar-professor/cadastrar-professor.component.ts
+++ src/app/faculdade/cadastrar-professor/cadastrar-professor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProfessorService } from '@app/_services';
 
 @Component({
   selector: 'app-cadastrar-professor',
@@ -8,25 +9,62 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./cadastrar-professor.component.less']
 })
 export class CadastrarProfessorComponent implements OnInit {
+  loading = false;
+  submitted = false;
+  error = false;
+  errorMessage = "";
   cadastrarProfessorForm: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
-) { 
-    
-}
+    private professorService: ProfessorService
+  ) {
+
+  }
 
   ngOnInit(): void {
     this.cadastrarProfessorForm = this.formBuilder.group({
-      email: ['', Validators.required]
-  });
+      email: ['', Validators.required],
+      nome: ['', Validators.required],
+      senha: ['', Validators.required]
+    });
   }
   get f() { return this.cadastrarProfessorForm.controls; }
 
   onSubmit() {
-    window.alert(this.f.email.value);
+    console.log('submit')
+    this.submitted = true;
+    if (this.cadastrarProfessorForm.invalid) {
+      return;
+    }
+    this.professorService.createProfessor({
+      email: this.f.email.value,
+      nome: this.f.nome.value,
+      senha: this.f.senha.value
+    }).subscribe(()=>{
+      document.getElementById("openModalButton").click();
+      this.limparFormulario();
+    }, (err)=>{
+      if(err == "Conflict"){
+        this.errorMessage = "Email ja utilizado!"
+      }else{
+        this.errorMessage = "Algo deu errado!"
+      }
+      this.error = true
+      setTimeout(()=>{
+        this.limparFormulario();
+        this.error = false
+      }, 2000)
+
+    })
+
     console.log()
+  }
+
+  private limparFormulario() {
+    this.submitted = false;
+    this.cadastrarProfessorForm.reset();
   }
 }
